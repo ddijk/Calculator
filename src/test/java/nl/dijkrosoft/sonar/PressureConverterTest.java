97154fe6e61addf51aa5db5046973e270df706f5
ddijk <dick@dijkrosoft.nl>
2017-06-22 20:48:44 +0200

    Add unit tests

---
+++ src/test/java/nl/dijkrosoft/sonar/PressureConverterTest.java
@@ -0,0 +1,25 @@
+package nl.dijkrosoft.sonar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dickdijk on 22/06/2017.
+ */
+public class PressureConverterTest {
+    @Test
+    public void barToHectoPascal() throws Exception {
+        PressureConverter pressureConverter = new PressureConverter();
+        double actualResult = pressureConverter.barToHectoPascal(0.9);
+        assertEquals(0.9, actualResult, 0.1);
+    }
+
+    @Test
+    public void hectoPascalToBar() throws Exception {
+        PressureConverter pressureConverter = new PressureConverter();
+        double actualResult = pressureConverter.hectoPascalToBar(0.9);
+        assertEquals(0.9, actualResult, 0.1);
+    }
+
+}
